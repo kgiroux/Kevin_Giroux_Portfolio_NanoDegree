b86ccfc0c0b2ca5328c85ba58718227947aa5de8
Giroux Kévin <giroux.kevin.fr@gmail.com>
2016-06-15 23:01:50 +0200

    Remove DEBUG flag

--- app/src/main/java/com/giroux/kevin/kevingirouxportfolio/database/MovieDbHelper.java
+++ app/src/main/java/com/giroux/kevin/kevingirouxportfolio/database/MovieDbHelper.java
@@ -15,7 +15,7 @@ import com.giroux.kevin.kevingirouxportfolio.database.MovieContractor.ReviewEntr
 public class MovieDbHelper extends SQLiteOpenHelper {
 
     static final String DATABASE_NAME = "movie.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public MovieDbHelper(Context context){
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
@@ -83,10 +83,10 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS" + FavoriteEntry.TABLE_NAME);
-            db.execSQL("DROP TABLE IF EXISTS" + MovieEntry.TABLE_NAME);
-            db.execSQL("DROP TABLE IF EXISTS" + TrailerEntry.TABLE_NAME);
-            db.execSQL("DROP TABLE IF EXISTS" + ReviewEntry.TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + FavoriteEntry.TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + TrailerEntry.TABLE_NAME);
+            db.execSQL("DROP TABLE IF EXISTS " + ReviewEntry.TABLE_NAME);
         }
         this.onCreate(db);
     }
