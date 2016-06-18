ce82335f0f1c0bcec8e5ea31438ad2e22aae1426
Giroux Kévin <giroux.kevin.fr@gmail.com>
2016-06-18 10:54:53 +0200

    Correction TRAILER CLICK

--- app/src/main/java/com/giroux/kevin/kevingirouxportfolio/ViewHolder/ViewHolderTrailer.java
+++ app/src/main/java/com/giroux/kevin/kevingirouxportfolio/ViewHolder/ViewHolderTrailer.java
@@ -1,6 +1,5 @@
 package com.giroux.kevin.kevingirouxportfolio.ViewHolder;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -43,25 +42,22 @@ public class ViewHolderTrailer extends RecyclerView.ViewHolder implements View.O
 
     @Override
     public void onClick(View v) {
+        Intent intent;
         if (v.getId() == R.id.linearLayout) {
-            try {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + trailer.getYoutubeKey()));
-
-                if(intent.resolveActivity(context.getPackageManager()) != null){
-                    context.startActivity(intent);
-                }
-
-
-            } catch (ActivityNotFoundException ex) {
-                Intent intent = new Intent(Intent.ACTION_VIEW,
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + trailer.getYoutubeKey()));
+            if(intent.resolveActivity(context.getPackageManager()) != null){
+                context.startActivity(intent);
+            } else {
+                intent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("http://www.youtube.com/watch?v=" + trailer.getYoutubeKey()));
-                if(intent.resolveActivity(context.getPackageManager()) != null) {
+                if (intent.resolveActivity(context.getPackageManager()) != null) {
 
                     context.startActivity(intent);
-                }else{
-                    Toast.makeText(context,context.getString(R.string.warningIntent),Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(context, context.getString(R.string.warningIntent), Toast.LENGTH_LONG).show();
                 }
             }
+
         }
 
     }
